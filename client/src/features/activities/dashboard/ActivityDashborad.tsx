099725b88522655e91566639c6f4f1400bcd8c0b
Siddharth Vaikunthe <siddsv7@gmail.com>
2025-06-30 10:24:46 +0530

    End of section 7

--- client/src/features/activities/dashboard/ActivityDashborad.tsx
+++ client/src/features/activities/dashboard/ActivityDashborad.tsx
@@ -1,44 +1,15 @@
 import { Grid2 } from "@mui/material";
 import ActivityList from "./ActivityList";
-import ActivityDetail from "../details/ActivityDetail";
-import ActivityForm from "../form/ActivityForm";
 
-type Props = {
-    activities: Activity[];
-    selectActivity: (id: string) => void;
-    cancelActivity: () => void;
-    selectedActivity?: Activity;
-    openForm: (id: string) => void;
-    closeForm: () => void;
-    editMode: boolean;
-}
-
-export default function ActivityDashborad({ activities,
-    selectActivity, cancelActivity, selectedActivity,
-    openForm, closeForm, editMode
-    }: Props) {
+export default function ActivityDashborad() {
+    
     return (
         <Grid2 container spacing={3}>
             <Grid2 size={7}>
-                <ActivityList
-                    activities={activities}
-                    selectActivity={selectActivity}
-                />
+                <ActivityList />
             </Grid2>
             <Grid2 size={5}>
-                {selectedActivity && !editMode &&
-                    <ActivityDetail
-                        selectedActivity={selectedActivity}
-                        cancelSelectActivity={cancelActivity}
-                        openForm={openForm}
-                    />
-                }
-                {editMode &&
-                    <ActivityForm
-                        closeForm={closeForm}
-                        activity={selectedActivity}
-                    />
-                }
+                Activity filters go here
             </Grid2>
         </Grid2>
     )
